2d099b2663ad54b015a1e4913b14f204874dc4b6
Leohurtado <lhurtadomanuel123@gmail.com>
2026-08-20 12:38:03 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+console.log("FinTrack está funcionando correctamente");
 
+const botonGasto = document.querySelector(".btn-primary");
+
+botonGasto.addEventListener("click", function () {
+
+    alert("Aquí vamos a registrar un nuevo gasto 💸");
+
+});
